ee8d26d33e24e8bc5a94a209c71fb0b1007c8adb
ebina4yaka <ebina4yaka@protonmail.com>
2026-09-09 21:57:17 +0000

    バージョン 0.2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "dev.example.jpkeyboard"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.2.5"
+        versionCode = 11
+        versionName = "0.2.6"
     }
 
     compileOptions {
